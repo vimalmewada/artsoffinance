45ee9144c22791e2a80473dfc01b145a1dfb54a8
vimalmewada <vimalmewada.vm@gmail.com>
2025-02-02 15:54:05 +0530

    update scpirt

--- src/shared/nav-bar/nav-bar.component.ts
+++ src/shared/nav-bar/nav-bar.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss']
+  styleUrls: ['./nav-bar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush // Add this
 })
+
 export class NavBarComponent {
 
 }
